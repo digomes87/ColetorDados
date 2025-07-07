ac2b66089e320b3f78e403cdf59c89e8976295f3
Diego Go <diego.gomes87@gmail.com>
2025-07-07 15:48:42 +0200

    Setup inicial para teste da aplicacao

---
+++ ResidentEvil/collect.py
@@ -0,0 +1,25 @@
+import requests
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    # 'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Referer': 'https://www.residentevildatabase.com/personagens/',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Priority': 'u=0, i',
+    # Requests doesn't support trailers
+    # 'TE': 'trailers',
+}
+url = "https://www.residentevildatabase.com/personagens/ada-wong/"
+response = requests.get(url=url, headers=headers)
+resp = requests.get(url)
+
+
+print(resp.status_code)
